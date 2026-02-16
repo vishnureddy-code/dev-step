18f3dbd911dcd01b1d1f971bbaa3de18a08a50e5
vishnureddy-code <pusapativishnuvardhanreddy@gmail.com>
2026-02-16 23:36:42 +0530

    UC2-assignment

---
+++ OOPSPrintBanner.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp
+ * UC2 Assignment - Print OOPS in ASCII Banner Format
+ * This program prints the word OOPS using asterisks in 7 lines.
+ * @author Vishnu
+ * @version 2.0
+ */
+
+public class OOPSPrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println(" *****    *****    *        ***** ");
+
+    }
+
+}
