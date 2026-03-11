01ce957de000981fcd7eff3bfb982d1f94d1c931
vishnureddy-code <pusapativishnuvardhanreddy@gmail.com>
2026-03-11 15:16:16 +0530

    UC3

---
+++ HelloNameDefault.java
@@ -0,0 +1,15 @@
+public class HelloNameDefault {
+
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
